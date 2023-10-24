16797fd09e3caa51569e1210b3a75faa6c57a641
kelasrayhan <rayhanrwa1@gmail.com>
2023-10-24 21:01:57 +0700

    Latihan 1 program input nilai

---
+++ main.c
@@ -0,0 +1,25 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+    
+    int mat, fis, geo;
+    
+    
+    printf("Masukan Nilai Matematika : ");
+    scanf("%d", &mat);
+    
+    printf("Masukan Nilai Fisika : ");
+    scanf("%d", &fis);
+    
+    printf("Masukan Nilai Geografi: ");
+    scanf("%d", &geo);
+
+
+    printf("\nMatematika : %d", mat);
+    printf("\nFisika : %d", fis);
+    printf("\nGeografi : %d", geo);
+    
+}
