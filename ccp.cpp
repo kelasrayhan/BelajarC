d5455d16c30c083a928bcc4fdcc33e32406f7b1c
kelasrayhan <rayhanrwa1@gmail.com>
2023-10-24 21:12:20 +0700

    Program Nilai

---
+++ ccp.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+    int mat, fis, geo;
+
+    std::cout << "Masukan Nilai Matematika : ";
+    std::cin >> mat;
+
+    std::cout << "Masukan Nilai Fisika : ";
+    std::cin >> fis;
+
+    std::cout << "Masukan Nilai Geografi: ";
+    std::cin >> geo;
+
+    std::cout << "\nMatematika : " << mat << std::endl;
+    std::cout << "Fisika : " << fis << std::endl;
+    std::cout << "Geografi : " << geo << std::endl;
+
+    return 0;
+}
